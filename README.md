a64be738821e8900dbe4053e5446e1d6bed0121a
Zane Durkin <zanedurkin@gmail.com>
2018-02-23 15:08:24 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+#stop_the_bot
+
+value: 50?
+description: Checkout the new website, you won't find it on google though
+falg: flag{stopping\_robots\_is\_easy}
---
+++ web/page.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Stop The Bot</title>
+</head>
+<body>
+  <div style="width:80%; margin-left:auto; margin-right:auto;">
+      <h3>Welcome to the site</h3>
+      <p>Have you found anything that catches your interest?</p>
+      <p>If not, keep looking</p>
+  </div>
+</body>
+</html>
